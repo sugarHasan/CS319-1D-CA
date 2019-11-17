eb3e4336f1f8c8ff0751c99c2b5fb0e23ed66709
BerkeOguz <56187966+BerkeOguz@users.noreply.github.com>
2019-11-17 20:45:39 +0300

    offer and trade

---
+++ src/OfferManager.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.ArrayList;
+public class OfferManager {
+    ArrayList<Offer> offers = new ArrayList<Offer>();
+    public void makeOffer(){
+
+    }
+    public void listOffers(ArrayList<Offer>offers){
+        for(int i = 0 ; i < offers.size() ; i++){
+            System.out.println( (i + 1) + "st Offer: " + offers.get(i) );
+        }
+    }
+    public boolean findOffer(){
+
+    }
+    public void closeOffer(){
+
+    }
+    public boolean acceptOffer(){
+
+    }
+    public boolean declineOffer(){
+
+    }
+}
