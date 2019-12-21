a2d6ec8d34da02719b66771e9012c2dbc9b6c89a
Alkım Önen <alkimonen@hotmail.com>
2019-12-21 17:38:09 +0300

    Happiness

---
+++ src/Model/Happiness.java
@@ -0,0 +1,53 @@
+package Model;
+
+public class Happiness {
+    private int value;
+
+    public Happiness()
+    {
+        value = 5;
+    }
+
+    public int getHappinessValue()
+    {
+        return value;
+    }
+
+    public void caughtFish( int number)
+    {
+        increaseHappinessValue( number);
+    }
+
+    public void robberInTown( int number)
+    {
+        decreaseHappinessValue( number);
+    }
+
+    public boolean canBuild()
+    {
+        if ( value > 2 )
+            return true;
+        return false;
+    }
+
+    public boolean loseCard()
+    {
+        if ( value == 0 )
+            return true;
+        return false;
+    }
+
+    public void increaseHappinessValue( int amount)
+    {
+        value += amount;
+        if ( value > 10 )
+            value = 10;
+    }
+
+    public void decreaseHappinessValue( int amount)
+    {
+        value -= amount;
+        if ( value < 0 )
+            value = 0;
+    }
+}
